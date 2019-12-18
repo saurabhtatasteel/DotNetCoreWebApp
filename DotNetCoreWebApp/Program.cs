0094b51f71e7c745c1ad16f5baf5cd99302d9af6
saurabhtatasteel <saurabh_tatasteel@yahoo.com>
2019-12-19 00:34:01 +0530

    Demo commit

--- DotNetCoreWebApp/Program.cs
+++ DotNetCoreWebApp/Program.cs
@@ -16,11 +16,12 @@ namespace DotNetCoreWebApp
 			CreateHostBuilder(args).Build().Run();
 		}
 
+
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+				  Host.CreateDefaultBuilder(args)
+					  .ConfigureWebHostDefaults(webBuilder =>
+					  {
+						  webBuilder.UseStartup<Startup>();
+					  });
 	}
 }
